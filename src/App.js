3956b32f46947d29fda227cff816892a319e0b67
Bandini2001 <bandinikohare30@gmail.com>
2023-11-25 01:24:31 +0530

    add players

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [player, setPlayer] = useState(1)
   return (
    <>
    <div>
    <img className='heading' src='https://4.bp.blogspot.com/-UGd-nOoMK5g/WBd6ICqoumI/AAAAAAAAHXI/_6oeNhPtdHUqi6iUStCLvUvDUGV1S82VQCLcB/s1600/tttmarqlrg.gif' />
 
+   <div className='player-container'>
+    <span className='players-title'>
+      Player 1:
+    </span>
+
+    <span>
+      Player 2:
+    </span>
+   </div>
+<div className='board'>
+  
 <div className='row'>
   <div className='box'></div>
   <div className='box'></div>
@@ -21,6 +34,7 @@ function App() {
   <div className='box'></div>
   <div className='box'></div>
   <div className='box'></div>
+</div>
 </div>
    </div>
    </>
